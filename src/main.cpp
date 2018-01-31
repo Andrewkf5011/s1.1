49fb2e65d8a468f8521935644b3b49fd8dec96ec
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-01-31 16:08:17 +0000

    Solution to wrapper for base K64F leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,40 @@
 #include <mbed.h>
 
+/* wrapper functions */
+/* Create an array of outputs for the actual Pins */
+DigitalOut led[] = { DigitalOut(LED_RED,1),
+                     DigitalOut(LED_GREEN,1),
+                     DigitalOut(LED_BLUE,1)
+};
+/* create an enum of _logical_ names */
+enum leds {Red, Green, Blue };
+/* Functions use the logical value as an array index to
+   access the physical pin */
+void  on(int l) { led[l].write(0); }
+void off(int l) { led[l].write(1); }
+
+
 int main() {
 
-    DigitalOut red(PTB22);
+    /* Version one */
+    //DigitalOut red(PTB22);
+    //DigitalOut green(PTE26);
+    //DigitalOut blue(PTB21);
 
+    /* From https://os.mbed.com/teams/Freescale/wiki/frdm-k64f-pinnames */
+    DigitalOut   red(LED_RED,  1); /* create with initial value of 1=>LED off */
+    DigitalOut green(LED_GREEN,1);
+    DigitalOut  blue(LED_BLUE, 1);
+
+    LED b(LED_BLUE);
     while(1) {
       red.write(0);
+      off(Green);
+      b.on();
       wait(0.5);
       red.write(1);
+      on(Green);
+      b.off();
       wait(0.5);
     }
 }
